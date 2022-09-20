e2f7ef900ae677bb725f64975657416817a5c297
vinhthanh <vinhthanhluong@gmail.com>
2022-09-20 17:31:50 +0700

    update header 2section

--- js/modules/top.js
+++ js/modules/top.js
@@ -4,9 +4,10 @@ $(function () {
 
     var obj = {
         init: function () {
-            this.aos();
-            this.wow();
-            // this.visual();
+            // this.aos();
+            // this.wow();
+            this.visual();
+            // this.loadPost();
         },
 
         //aos
@@ -41,7 +42,7 @@ $(function () {
         visual: function () {
             if ($('#visual').length > 0) {
                 $('#visual').slick({
-                    dots: false,
+                    dots: true,
                     infinite: true,
                     speed: 1000,
                     slidesToShow: 1,
@@ -52,11 +53,32 @@ $(function () {
                     centerMode: false,
                     centerPadding: 0,
                     pauseOnHover: false,
+                    pauseOnFocus:false,
                     fade: false,
                     variableWidth: false,
                 });
             }
         },
+
+        loadPost: function () {
+            $.ajax({
+                url: 'blog/_custom/?limit=2',
+                dataType: 'jsonp',
+                success: function (json) {
+                    $.each(json.data, function (i, val) {
+                        var dateFormatted = val.date.replace('/', '.').replace('/', '.');
+                        var items =
+                            '<li>' +
+                            '<a href="./blog/' + val.url + '" class="blogs-item">' +
+                            '<span class="blogs-time">' + dateFormatted + '</span>' +
+                            '<span class="blogs-tt">' + val.title + '</span>' +
+                            '</a>' +
+                            '</li>'
+                        $('#blog-list').append(items);
+                    });
+                }
+            });
+        }
     }
 
     obj.init();
